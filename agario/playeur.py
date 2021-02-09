c9e9aaaf2e0b39f3eb9bedaffa06daf649770c10
Landry-goncalves <landry.gon@outlook.fr>
2021-02-09 10:04:46 +0100

    agario1

---
+++ agario/playeur.py
@@ -0,0 +1,19 @@
+class playeur:
+
+    def __init__(self):
+        self.size = ()
+        self.vitesse = ()
+        self.forme = ()
+        self.couleur = vector3()
+        self.position = vector2()
+        self.direction = vector2()
+
+    def manger (self):
+        self.size[1] = self.size[1]+self.size[1]
+
+    def mourir (self):
+
+    def deplacer (self):
+
+    def afficher (self):
+        
